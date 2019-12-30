f03761b4df79e9da911b5d17108323d4412fa980
vickywane <vickywane@gmail.com>
2019-12-30 11:46:02 +0000

    ..

--- src/pages/home/features/organizers.tsx
+++ src/pages/home/features/organizers.tsx
@@ -10,12 +10,41 @@ const Title = styled.h4`
   color: blue;
 `
 
+const Contain = styled.div`
+  padding: 0.5em;
+`
+
 const Organizers = () => {
   return (
     <Body>
       <Title style={{ textAlign: "left", paddingLeft: "10px" }}>
         {" "}
         Event for Organizers{" "}
+        <Flex justifyBetween>
+          <img src="../../../assets/ipad.svg" alt="ipad" />
+
+          <Contain>
+            <Flex column>
+              <Title style={{ textAlign: "right", fontSize: "1.2em" }}>
+                {" "}
+                Create and Manage Events
+              </Title>
+              <br />
+
+              <Title style={{ textAlign: "right", fontSize: "1.2em" }}>
+                {" "}
+                Store and Manage Event Assets{" "}
+              </Title>
+              <br />
+
+              <Title style={{ textAlign: "right", fontSize: "1.2em" }}>
+                {" "}
+                Co-ordinate Collaborators{" "}
+              </Title>
+              <br />
+            </Flex>
+          </Contain>
+        </Flex>
       </Title>
     </Body>
   )
